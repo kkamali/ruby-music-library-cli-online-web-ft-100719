43bb0c1bada35a4eb90b79b344c7aff3796d53f5
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-22 00:04:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,4 +44,8 @@ class MusicLibraryController
       songs.each_with_index{|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
     end
   end 
+  
+  def list_songs_by_genre
+    puts "Please enter the genre:"
+  end 
 end 
