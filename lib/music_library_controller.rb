9ec84dd9698d4fd34bf6e6935938ea892b182959
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-22 00:10:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -59,6 +59,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     user_input = gets.chomp
     if user_input.to_i > 0 && user_input.to_i < Song.all.count
+      sorted = Song.all.sort{|song1, song2| song1.name <=> song2.name}
       puts "Playing #{Song.all[user_input.to_i - 1].name} by #{Song.all[user_input.to_i - 1].artist.name}"
     end
   end 
